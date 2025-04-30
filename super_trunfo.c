55ae386886e748b28d692c54abda573d49f93d59
PauloGG1 <pgaspar1612@gmail.com>
2025-04-30 12:46:53 +0000

    Criei variáveis para armazenar os dados das cartas

--- super_trunfo.c
+++ super_trunfo.c
@@ -5,13 +5,7 @@
 // Teste PauloGG1
 
 int main() {
-   // Variáveis para armazenar os dados das carta
-   char codigo[5]; // Ex: A01
-   char nome[50];
-   int população;
-   float area;
-   float pib;
-   int pontosTuristicos;
+ 
 
 
 
