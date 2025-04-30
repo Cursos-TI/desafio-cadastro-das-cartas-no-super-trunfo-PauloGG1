ee2d58287f1a155e33908def1995c0a02340902f
PauloGG1 <pgaspar1612@gmail.com>
2025-04-30 12:44:22 +0000

    criei variáveis para armazenar os dados das cartas

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 
+// Desafio Super Trunfo - Países
+// Desafio Novato - Cadastro das Cartas 
+// Teste PauloGG1
+
 int main() {
-   
+   // Variáveis para armazenar os dados das carta
+   char codigo[5]; // Ex: A01
+   char nome[50];
+   int população;
+   float area;
+   float pib;
+   int pontosTuristicos;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
