e4a1138ecdbd6f0987a14c90cc0445c40e9f841d
PauloGG1 <pgaspar1612@gmail.com>
2025-05-07 12:18:09 +0000

    fiz os dados de duas cartas

---
+++ super_trunfo desafio da logica.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// Desafio Super Trunfo -Desafio da logica - comparaçao de cartas
+// Desafio Novato - comparaçao de cartas
+// Teste PauloGG1
+
+int main() {
+   // Variáveis para armazenar os dados da carta um
+   char estado1[] = "Amazonas";
+   char codigo1[] = "A01";
+   char nomeCidade1[] = "Manaus";
+   int populacao1 = 1860289;  
+   float area1 = 1570745.7;   
+   float pib1 = 124.5;        
+   int pontosTuristicos1 = 7;
+
+   // Variáveis para armazenar os dados da carta dois
+   char estado2[] = "Bahia";
+   char codigo2[] = "B01";
+   char nomeCidade2[] = "Salvador";
+   int populacao2 = 2886698;
+   float area2 = 1553.0;     
+   float pib2 = 96.2;        
+   int pontosTuristicos2 = 12;
+
+ 
